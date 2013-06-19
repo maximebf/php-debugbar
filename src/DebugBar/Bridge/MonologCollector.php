ed89ca4a56624ef010e1576511f81ff660ca3e41
maximebf <maxime.bouroumeau@gmail.com>
2013-06-19 11:09:40 +0900

    added missing call to parent constructor

--- src/DebugBar/Bridge/MonologCollector.php
+++ src/DebugBar/Bridge/MonologCollector.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the DebugBar package.
+ *
+ * (c) 2013 Maxime Bouroumeau-Fuseau
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace DebugBar\Bridge;
 
@@ -20,6 +28,7 @@ class MonologCollector extends AbstractProcessingHandler implements DataCollecto
 
     public function __construct(Logger $logger, $level = Logger::DEBUG, $bubble = true)
     {
+        parent::__construct($level, $bubble);
         $logger->pushHandler($this);
     }
 
