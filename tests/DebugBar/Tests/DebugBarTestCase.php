8fde2e9a0febd77aaf75f95efd7a7aa5c721943d
maximebf <maxime.bouroumeau@gmail.com>
2013-08-14 22:22:07 +1000

    added tests

--- tests/DebugBar/Tests/DebugBarTestCase.php
+++ tests/DebugBar/Tests/DebugBarTestCase.php
@@ -11,4 +11,35 @@ abstract class DebugBarTestCase extends \PHPUnit_Framework_TestCase
     {
         $this->debugbar = new DebugBar();
     }
+
+    public function assertJsonIsArray($json)
+    {
+        $data = json_decode($json);
+        $this->assertTrue(is_array($data));
+    }
+
+    public function assertJsonIsObject($json)
+    {
+        $data = json_decode($json);
+        $this->assertTrue(is_object($data));
+    }
+
+    public function assertJsonArrayNotEmpty($json)
+    {
+        $data = json_decode($json, true);
+        $this->assertTrue(is_array($data) && !empty($data));
+    }
+
+    public function assertJsonHasProperty($json, $property)
+    {
+        $data = json_decode($json, true);
+        $this->assertTrue(array_key_exists($property, $data));
+    }
+
+    public function assertJsonPropertyEquals($json, $property, $expected)
+    {
+        $data = json_decode($json, true);
+        $this->assertTrue(array_key_exists($property, $data));
+        $this->assertEquals($expected, $data[$property]);
+    }
 }
