1663f3808b41b7a22b2bc453f4751664df422d7c
Barry vd. Heuvel <barryvdh@gmail.com>
2014-01-14 09:52:37 +0100

    Improve filtering
    Make filter work with multiple keys and wildcard search, eg. user/* or ip 10.0.*

--- src/DebugBar/Storage/FileStorage.php
+++ src/DebugBar/Storage/FileStorage.php
@@ -77,7 +77,7 @@ class FileStorage implements StorageInterface
             $data = $this->get($file['id']);
             $meta = $data['__meta'];
             unset($data);
-            if (array_keys(array_intersect($meta, $filters)) == array_keys($filters)) {
+            if ($this->filter($meta, $filters)) {
                 $results[] = $meta;
             }
             if(count($results) >= ($max + $offset)){
@@ -86,6 +86,15 @@ class FileStorage implements StorageInterface
         }
 
         return array_slice($results, $offset, $max);
+    }
+    
+    protected function filter($meta, $filters){
+        foreach($filters as $key => $value){
+            if(fnmatch ($value, $meta[$key]) === false){
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
