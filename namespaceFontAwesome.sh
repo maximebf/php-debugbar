5837cd8f542b4c2b70ad1c967ba2c9a454f57bb9
Nick Colley <nick.colley@drpgroup.com>
2015-08-28 12:41:40 +0100

    Also namespace fontfamily name

--- namespaceFontAwesome.sh
+++ namespaceFontAwesome.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sed -i 's/FontAwesome/PhpDebugbarFontAwesome/g' src/DebugBar/Resources/vendor/font-awesome/css/font-awesome.min.css
 sed -i 's/fa-/phpdebugbar-fa-/g' src/DebugBar/Resources/vendor/font-awesome/css/font-awesome.min.css
 sed -i 's/\.fa/\.phpdebugbar-fa/g' src/DebugBar/Resources/vendor/font-awesome/css/font-awesome.min.css
