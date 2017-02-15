73acfcbfc25d024396640227c5aa312117265628
Barry vd. Heuvel <barryvdh@gmail.com>
2017-02-15 09:59:22 +0100

    Update TimeDataCollector.php

--- src/DebugBar/DataCollector/TimeDataCollector.php
+++ src/DebugBar/DataCollector/TimeDataCollector.php
@@ -225,7 +225,7 @@ class TimeDataCollector extends DataCollector implements Renderable
         $headers = [];
 
         foreach ($this->collect()['measures'] as $k => $m) {
-            $headers[] = sprintf('%d=%f; "%s"', $k, $m['duration'], str_replace('"', "'", $m['label']));
+            $headers[] = sprintf('%d=%F; "%s"', $k, $m['duration'], str_replace('"', "'", $m['label']));
         }
 
         return $headers;
