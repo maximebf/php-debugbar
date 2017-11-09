aec220292c7507abe645a9adbbe7f84889425ee4
Tim Riemenschneider <git@tim-riemenschneider.de>
2017-11-09 22:32:48 +0100

    Fix & enable xdebug-link-generation in ExceptionsCollector

--- src/DebugBar/Resources/widgets.js
+++ src/DebugBar/Resources/widgets.js
@@ -490,7 +490,17 @@ if (typeof(PhpDebugBar) == 'undefined') {
             this.$list = new ListWidget({ itemRenderer: function(li, e) {
                 $('<span />').addClass(csscls('message')).text(e.message).appendTo(li);
                 if (e.file) {
-                    $('<span />').addClass(csscls('filename')).text(e.file + "#" + e.line).appendTo(li);
+                    var header = $('<span />').addClass(csscls('filename')).text(e.file + "#" + e.line);
+                    if (e.xdebug_link) {
+                        if (e.xdebug_link.ajax) {
+                            $('<a title="' + e.xdebug_link.url + '"></a>').on('click', function () {
+                                $.ajax(e.xdebug_link.url);
+                            }).addClass(csscls('editor-link')).appendTo(header);
+                        } else {
+                            $('<a href="' + e.xdebug_link.url + '"></a>').addClass(csscls('editor-link')).appendTo(header);
+                        }
+                    }
+                    header.appendTo(li);
                 }
                 if (e.type) {
                     $('<span />').addClass(csscls('type')).text(e.type).appendTo(li);
