2c59581d4e7116498c11fd1756a8335a547730da
Leonardo Bressan Motyczka <leomoty@gmail.com>
2019-10-09 13:47:31 -0300

    Update twig demo to use TwigProfileCollector

--- demo/bridge/twig/index.php
+++ demo/bridge/twig/index.php
@@ -6,9 +6,11 @@ include __DIR__ . '/../../bootstrap.php';
 $debugbarRenderer->setBaseUrl('../../../src/DebugBar/Resources');
 
 $loader = new Twig_Loader_Filesystem('.');
-$twig = new DebugBar\Bridge\Twig\TraceableTwigEnvironment(new Twig_Environment($loader), $debugbar['time']);
+$twig = new Twig_Environment($loader);
+$profile = new Twig_Profiler_Profile();
+$twig->addExtension(new DebugBar\Bridge\Twig\TimeableTwigExtensionProfiler($profile, $debugbar['time']));
 
-$debugbar->addCollector(new DebugBar\Bridge\Twig\TwigCollector($twig));
+$debugbar->addCollector(new DebugBar\Bridge\TwigProfileCollector($profile));
 
 render_demo_page(function() use ($twig) {
     echo $twig->render('hello.html', array('name' => 'peter pan'));
