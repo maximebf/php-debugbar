40f375504a4dd8e59f779c3f3cac524777ddcde5
Barry vd. Heuvel <barryvdh@gmail.com>
2020-04-16 11:05:52 +0200

    Skip decimals if < 100ms

--- src/DebugBar/DataFormatter/DataFormatter.php
+++ src/DebugBar/DataFormatter/DataFormatter.php
@@ -54,8 +54,10 @@ class DataFormatter implements DataFormatterInterface
     {
         if ($seconds < 0.001) {
             return round($seconds * 1000000) . 'μs';
-        } elseif ($seconds < 1) {
+        } elseif ($seconds < 0.1) {
             return round($seconds * 1000, 2) . 'ms';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000) . 'ms';
         }
         return round($seconds, 2) . 's';
     }
