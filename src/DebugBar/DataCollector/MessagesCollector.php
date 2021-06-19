7602079356fc344f388a4e920bd663d272b82695
Pierre Rudloff <contact@rudloff.pro>
2021-06-19 19:08:10 +0200

    Add support for PSR-3 message placeholders (#465)

--- src/DebugBar/DataCollector/MessagesCollector.php
+++ src/DebugBar/DataCollector/MessagesCollector.php
@@ -185,7 +185,29 @@ class MessagesCollector extends AbstractLogger implements DataCollectorInterface
      */
     public function log($level, $message, array $context = array())
     {
-        $this->addMessage($message, $level);
+        $this->addMessage($this->interpolate($message, $context), $level);
+    }
+
+    /**
+     * Interpolates context values into the message placeholders.
+     *
+     * @param $message
+     * @param array $context
+     * @return string
+     */
+    function interpolate($message, array $context = array())
+    {
+        // build a replacement array with braces around the context keys
+        $replace = array();
+        foreach ($context as $key => $val) {
+            // check that the value can be cast to string
+            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
+                $replace['{' . $key . '}'] = $val;
+            }
+        }
+
+        // interpolate replacement values into the message and return
+        return strtr($message, $replace);
     }
 
     /**
