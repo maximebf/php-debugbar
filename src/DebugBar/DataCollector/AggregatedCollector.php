c2e310320b7751a959f1590bb76530abbcfd2ce6
rowhel95 <rowhel95@live.nl>
2022-02-11 12:14:20 +0100

    PHP 8.1 fix (#495)
    * PHP 8.1 fix
    
    * Use supported return types

--- src/DebugBar/DataCollector/AggregatedCollector.php
+++ src/DebugBar/DataCollector/AggregatedCollector.php
@@ -155,7 +155,7 @@ class AggregatedCollector implements DataCollectorInterface, ArrayAccess
      * @param mixed $value
      * @throws DebugBarException
      */
-    public function offsetSet($key, $value)
+    public function offsetSet($key, $value): void
     {
         throw new DebugBarException("AggregatedCollector[] is read-only");
     }
@@ -164,6 +164,8 @@ class AggregatedCollector implements DataCollectorInterface, ArrayAccess
      * @param mixed $key
      * @return mixed
      */
+    
+    #[\ReturnTypeWillChange]
     public function offsetGet($key)
     {
         return $this->collectors[$key];
@@ -173,7 +175,7 @@ class AggregatedCollector implements DataCollectorInterface, ArrayAccess
      * @param mixed $key
      * @return bool
      */
-    public function offsetExists($key)
+    public function offsetExists($key): bool
     {
         return isset($this->collectors[$key]);
     }
@@ -182,7 +184,7 @@ class AggregatedCollector implements DataCollectorInterface, ArrayAccess
      * @param mixed $key
      * @throws DebugBarException
      */
-    public function offsetUnset($key)
+    public function offsetUnset($key): void
     {
         throw new DebugBarException("AggregatedCollector[] is read-only");
     }
