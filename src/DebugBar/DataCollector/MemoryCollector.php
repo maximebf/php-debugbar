c95d0ae83aa2963d01a4961a90dca9d873589d9e
Andrew Brown <browner12@gmail.com>
2023-03-08 18:13:25 -0600

    allow customization of the memory precision
    currently there is no way to see the raw "peak_usage" bytes in the output. I'd love to have that number available somewhere, but I couldn't figure out how to do that.
    
    this change is the next best thing, as it allows us to override the default precision of "0", so we can get a little more granularity into the memory usage.

--- src/DebugBar/DataCollector/MemoryCollector.php
+++ src/DebugBar/DataCollector/MemoryCollector.php
@@ -19,6 +19,18 @@ class MemoryCollector extends DataCollector implements Renderable
 
     protected $peakUsage = 0;
 
+    protected $precision = 0;
+
+    /**
+     * Set the precision of the 'peak_usage_str' output.
+     *
+     * @param int $precision
+     */
+    public function setPrecision($precision)
+    {
+        $this->precision = $precision;
+    }
+
     /**
      * Returns whether total allocated memory page size is used instead of actual used memory size
      * by the application.  See $real_usage parameter on memory_get_peak_usage for details.
@@ -67,7 +79,7 @@ class MemoryCollector extends DataCollector implements Renderable
         $this->updatePeakUsage();
         return array(
             'peak_usage' => $this->peakUsage,
-            'peak_usage_str' => $this->getDataFormatter()->formatBytes($this->peakUsage, 0)
+            'peak_usage_str' => $this->getDataFormatter()->formatBytes($this->peakUsage, $this->precision)
         );
     }
 
