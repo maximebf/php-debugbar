10f137f76d38814ece9473962bc12cda881ca32e
erikn69 <erikn_69@hotmail.com>
2024-02-19 14:33:14 -0500

    Fix twig demo

--- demo/bridge/twig/index.php
+++ demo/bridge/twig/index.php
@@ -5,12 +5,12 @@ include __DIR__ . '/../../bootstrap.php';
 
 $debugbarRenderer->setBaseUrl('../../../src/DebugBar/Resources');
 
-$loader = new Twig_Loader_Filesystem('.');
-$twig = new Twig_Environment($loader);
-$profile = new Twig_Profiler_Profile();
+$loader = new Twig\Loader\FilesystemLoader('.');
+$twig = new Twig\Environment($loader);
+$profile = new Twig\Profiler\Profile();
 $twig->addExtension(new DebugBar\Bridge\Twig\TimeableTwigExtensionProfiler($profile, $debugbar['time']));
 
-$debugbar->addCollector(new DebugBar\Bridge\TwigProfileCollector($profile));
+$debugbar->addCollector(new DebugBar\Bridge\NamespacedTwigProfileCollector($profile, $twig));
 
 render_demo_page(function() use ($twig) {
     echo $twig->render('hello.html', array('name' => 'peter pan'));
