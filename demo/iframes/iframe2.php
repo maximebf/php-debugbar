c7d2cfba153139c40f757adff58e0f29ac4c8cd8
Barry vd. Heuvel <barryvdh@gmail.com>
2024-03-11 15:49:43 +0100

    Revert "keeps only the main debugbar functionality on iframes (#606)" (#613)
    This reverts commit b6113ae0e536dc3fe2a4bc8b8df53a84ae8427cc.

--- demo/iframes/iframe2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include '../bootstrap.php';
-
-$debugbarRenderer->setBaseUrl('../../src/DebugBar/Resources');
-
-$debugbar['messages']->addMessage('I\'m a Deeper Hidden Iframe');
-
-render_demo_page(function() {});
