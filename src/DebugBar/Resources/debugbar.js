73ea8d944b1f92aee848b3969636ee48011799eb
Barry vd. Heuvel <barryvdh@gmail.com>
2024-03-11 15:29:09 +0100

    Revert "keeps only the main debugbar functionality on iframes (#606)"
    This reverts commit b6113ae0e536dc3fe2a4bc8b8df53a84ae8427cc.

--- src/DebugBar/Resources/debugbar.js
+++ src/DebugBar/Resources/debugbar.js
@@ -424,7 +424,6 @@ if (typeof(PhpDebugBar) == 'undefined') {
             this.activePanelName = null;
             this.datesetTitleFormater = new DatasetTitleFormater(this);
             this.options.bodyMarginBottomHeight = parseInt($('body').css('margin-bottom'));
-            this.hasParent = window.parent && window.parent.phpdebugbar && window.parent.phpdebugbar != this;
             this.registerResizeHandler();
         },
 
@@ -434,7 +433,7 @@ if (typeof(PhpDebugBar) == 'undefined') {
          * @this {DebugBar}
          */
         registerResizeHandler: function() {
-            if (typeof this.resize.bind == 'undefined' || this.hasParent) return;
+            if (typeof this.resize.bind == 'undefined') return;
 
             var f = this.resize.bind(this);
             this.respCSSSize = 0;
@@ -475,10 +474,6 @@ if (typeof(PhpDebugBar) == 'undefined') {
          * @this {DebugBar}
          */
         render: function() {
-            if (this.hasParent) {
-                this.$el.hide();
-            }
-
             var self = this;
             this.$el.appendTo('body');
             this.$dragCapture = $('<div />').addClass(csscls('drag-capture')).appendTo(this.$el);
@@ -578,7 +573,6 @@ if (typeof(PhpDebugBar) == 'undefined') {
          * @this {DebugBar}
          */
         restoreState: function() {
-            if (this.hasParent) return;
             // bar height
             var height = localStorage.getItem('phpdebugbar-height');
             this.setHeight(height || this.$body.height());
@@ -929,15 +923,6 @@ if (typeof(PhpDebugBar) == 'undefined') {
          * @return {String} Dataset's id
          */
         addDataSet: function(data, id, suffix, show) {
-            if (this.hasParent) {
-                if (!suffix || ('(iframe)').indexOf(suffix) < 0) {
-                    suffix = '(iframe)' + (suffix || '');
-                }
-                
-                window.parent.phpdebugbar.addDataSet(data, id, suffix, show);
-                return;
-            }
-
             var label = this.datesetTitleFormater.format(id, data, suffix);
             id = id || (getObjectSize(this.datasets) + 1);
             this.datasets[id] = data;
