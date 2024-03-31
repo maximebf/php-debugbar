b0d46155099703e7f3501c5576475ffc2477db5d
Barry vd. Heuvel <barry@fruitcake.nl>
2024-03-31 19:07:50 +0200

    Add ajax test

--- tests/DebugBar/Browser/BasicBrowserTest.php
+++ tests/DebugBar/Browser/BasicBrowserTest.php
@@ -10,23 +10,21 @@ use DebugBar\RandomRequestIdGenerator;
 use Facebook\WebDriver\WebDriverElement;
 use Symfony\Component\Panther\PantherTestCase;
 
-class BasicBrowserTest extends PantherTestCase
+class BasicBrowserTest extends AbstractBrowserTest
 {
-    public function testDebugbar(): void
+    public function testDebugbarTab(): void
     {
-        // Start demo
         $client = static::createPantherClient();
-        $client->request('GET', '/demo');
+        $crawler = $client->request('GET', '/demo/');
 
         // Wait for Debugbar to load
         $crawler = $client->waitFor('.phpdebugbar-body');
 
-        $firstTab = $crawler->filter('a.phpdebugbar-tab')->link();
-        $client->click($firstTab);
+        $client->click($this->getTabLink($crawler, 'messages'));
 
-        $crawler = $client->waitForVisibility('.phpdebugbar-widgets-messages .phpdebugbar-widgets-list');
+        $crawler = $client->waitForVisibility('.phpdebugbar-panel[data-collector=messages] .phpdebugbar-widgets-list');
 
-        $messages = $crawler->filter('.phpdebugbar-widgets-messages .phpdebugbar-widgets-value')
+        $messages = $crawler->filter('.phpdebugbar-panel[data-collector=messages] .phpdebugbar-widgets-value')
             ->each(function(WebDriverElement $node){
                 return $node->getText();
             });
@@ -34,9 +32,40 @@ class BasicBrowserTest extends PantherTestCase
         $this->assertEquals('hello', $messages[0]);
         $this->assertCount(4, $messages);
 
-        $firstTab = $crawler->filter('a.phpdebugbar-tab')->link();
-        $client->click($firstTab);
-        $client->waitForInvisibility('.phpdebugbar-widgets-messages .phpdebugbar-widgets-list');
-
+        // Close it again
+        $client->click($this->getTabLink($crawler, 'messages'));
+        $client->waitForInvisibility('.phpdebugbar-panel[data-collector=messages] .phpdebugbar-widgets-list');
     }
+
+    public function testDebugbarAjax(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/demo/');
+
+        // Wait for Debugbar to load
+        $crawler = $client->waitFor('.phpdebugbar-body');
+
+        $client->click($this->getTabLink($crawler, 'messages'));
+
+        $crawler = $client->waitForVisibility('.phpdebugbar-widgets-messages .phpdebugbar-widgets-list');
+
+        $crawler->selectLink('load ajax content')->click();
+        $client->waitForElementToContain('.phpdebugbar-panel[data-collector=messages]', 'hello from ajax');
+        $client->waitForElementToContain('.phpdebugbar-datasets-switcher', 'ajax.php');
+
+        $messages = $crawler->filter('.phpdebugbar-panel[data-collector=messages] .phpdebugbar-widgets-value')
+            ->each(function(WebDriverElement $node){
+                return $node->getText();
+            });
+
+        $this->assertEquals('hello from ajax', $messages[0]);
+
+        $crawler->selectLink('load ajax content with exception')->click();
+
+        $client->click($this->getTabLink($crawler, 'exceptions'));
+
+        $client->waitForElementToContain('.phpdebugbar-datasets-switcher', 'ajax_exception.php');
+        $client->waitForElementToContain('.phpdebugbar-panel[data-collector=exceptions] .phpdebugbar-widgets-message', 'Something failed!');
+    }
+
 }
