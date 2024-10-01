134c7c676b7eebbc0fcee0a70aeb13254f0d55bb
Jesper Noordsij <jesper@sslleiden.nl>
2024-10-01 12:10:39 +0200

    Replace implicit nullable types in parameter declarations

--- src/DebugBar/DataCollector/PDO/TracedStatement.php
+++ src/DebugBar/DataCollector/PDO/TracedStatement.php
@@ -57,7 +57,7 @@ class TracedStatement
      * @param float $endTime
      * @param int $endMemory
      */
-    public function end(\Exception $exception = null, int $rowCount = 0, float $endTime = null, int $endMemory = null) : void
+    public function end(?\Exception $exception = null, int $rowCount = 0, ?float $endTime = null, ?int $endMemory = null) : void
     {
         $this->endTime = $endTime ?: microtime(true);
         $this->duration = $this->endTime - $this->startTime;
